06744cf33fc28e1abec97b559bd6f6e2ba26f2bd
Daniel Escalona <daniel07escalona@gmail.com>
2022-02-01 12:40:49 -0500

    commit tree f7248b5dc66633f0b0f7cc827511c271eac975af

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // Add your code here
+
+function submitData(user, email){
+  return fetch("http://localhost:3000/users", {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json",
+    Accept: "application/json",
+  },
+  body: JSON.stringify({
+    name: `${user}`,
+    email: `${email}`,
+  }),
+})
+.then(res => res.json())
+.then(usuario => appendToDom(usuario.id))
+.catch(error => appendToDom(error.message))
+
+}
+
+function deleteObj(id){
+  fetch(`http://localhost:3000/users/${id}`, {
+  method: "DELETE",
+  headers: {
+    "Content-Type": "application/json"
+  }
+})
+.then(res => res.json())
+.then( obj => console.log(obj))
+}
+
+function appendToDom(value){
+  let p = document.createElement('p');
+  p.textContent = `${value}`
+  let body = document.querySelector('body')
+  body.appendChild(p)
+}
+
+
